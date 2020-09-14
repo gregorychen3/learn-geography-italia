1a131580cb680d77da3a7d8c4cc7be8bae43f0ce
Gregory Chen <gregorychen3@gmail.com>
2020-09-13 22:11:11 -0400

    overview route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { Button } from "@material-ui/core";
 import React from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import Overview from "./pages/Overview";
 
 export default function App() {
   return (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+    <Switch>
+      <Route path="/overview">
+        <Overview />
+      </Route>
+      <Route path="/">
+        <Redirect to="/overview" />
+      </Route>
+    </Switch>
   );
 }
---
+++ src/pages/Overview.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@material-ui/core";
+import React from "react";
+
+export default function Overview() {
+  return (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  );
+}
