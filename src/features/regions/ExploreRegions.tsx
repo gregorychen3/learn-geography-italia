f7991c1b707f5ac4dbb8c5b615ba9f4e44107ee7
Gregory Chen <gregorychen3@gmail.com>
2020-09-14 01:39:31 -0400

    pull region info from wikipedia

--- src/features/regions/ExploreRegions.tsx
+++ src/features/regions/ExploreRegions.tsx
@@ -2,27 +2,44 @@ import { Grid, Typography } from "@material-ui/core";
 import Italy from "@svg-maps/italy";
 import React, { useState } from "react";
 import { RadioSVGMap } from "react-svg-map";
+import wiki from "wikijs";
 
 interface RegionInfo {
-  name: string;
+  officialName: string;
+  leaderName: string;
+  leaderParty: string;
+  leaderTitle: string;
+  populationAsOf: number;
+  populationTotal: number;
+  seat: string;
+  seatTyle: string;
+  mainImage: string;
 }
 
 export default function ExploreRegions() {
   const [regionInfo, setRegionInfo] = useState<RegionInfo | undefined>();
 
+  const handleMouseOver = async (e: any) => {
+    const target: SVGGraphicsElement = e.target;
+    const region = target.getAttribute("name");
+    if (!region) {
+      return;
+    }
+    wiki()
+      .page(region)
+      .then((page) => page.info())
+      .then((info) => setRegionInfo(info as RegionInfo));
+  };
+
   return (
     <Grid container>
       <Grid item xs={12} sm={6}>
-        <Typography align="center">{regionInfo}</Typography>
+        <Typography align="center">
+          <pre>{JSON.stringify(regionInfo, null, 2)}</pre>
+        </Typography>
       </Grid>
       <Grid item xs={12} sm={6}>
-        <RadioSVGMap
-          map={Italy}
-          onLocationMouseOver={(e) => {
-            const target: SVGGraphicsElement = e.target;
-            setRegionInfo({ name: e.name });
-          }}
-        />
+        <RadioSVGMap map={Italy} onLocationMouseOver={handleMouseOver} />
       </Grid>
     </Grid>
   );
