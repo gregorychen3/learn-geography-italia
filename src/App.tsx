fec6d5f447471fcc7571fad506fab675f014574d
Gregory Chen <gregorychen3@gmail.com>
2020-09-13 22:14:46 -0400

    move css baseline to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
+import { CssBaseline } from "@material-ui/core";
 import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Overview from "./pages/Overview";
 
 export default function App() {
   return (
-    <Switch>
-      <Route path="/overview">
-        <Overview />
-      </Route>
-      <Route path="/">
-        <Redirect to="/overview" />
-      </Route>
-    </Switch>
+    <>
+      <CssBaseline />
+      <Switch>
+        <Route path="/overview">
+          <Overview />
+        </Route>
+        <Route path="/">
+          <Redirect to="/overview" />
+        </Route>
+      </Switch>
+    </>
   );
 }
--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -5,7 +5,6 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Container from "@material-ui/core/Container";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import Grid from "@material-ui/core/Grid";
 import Link from "@material-ui/core/Link";
 import { makeStyles } from "@material-ui/core/styles";
@@ -66,7 +65,6 @@ export default function Overview() {
 
   return (
     <>
-      <CssBaseline />
       <AppBar position="relative">
         <Toolbar>
           <ExploreIcon className={classes.icon} />
