924eda6e45a9a5f422b081158bc0ba785ef0b52e
Gregory Chen <gregorychen3@gmail.com>
2020-09-14 02:05:09 -0400

    region card

--- src/features/regions/ExploreRegions.tsx
+++ src/features/regions/ExploreRegions.tsx
@@ -1,42 +1,30 @@
-import { Grid, Typography } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import Italy from "@svg-maps/italy";
 import React, { useState } from "react";
 import { RadioSVGMap } from "react-svg-map";
 import wiki from "wikijs";
-
-interface RegionInfo {
-  officialName: string;
-  leaderName: string;
-  leaderParty: string;
-  leaderTitle: string;
-  populationAsOf: number;
-  populationTotal: number;
-  seat: string;
-  seatTyle: string;
-  mainImage: string;
-}
+import RegionCard from "./RegionCard";
+import { RegionInfo } from "./types";
 
 export default function ExploreRegions() {
   const [regionInfo, setRegionInfo] = useState<RegionInfo | undefined>();
 
   const handleMouseOver = async (e: any) => {
-    const target: SVGGraphicsElement = e.target;
-    const region = target.getAttribute("name");
+    const region = e.target.getAttribute("name");
     if (!region) {
       return;
     }
-    wiki()
-      .page(region)
-      .then((page) => page.info())
-      .then((info) => setRegionInfo(info as RegionInfo));
+
+    const regionPage = await wiki().page(region);
+    const info = await regionPage.info();
+    const mainImage = await regionPage.mainImage();
+    setRegionInfo({ ...info, mainImage } as RegionInfo);
   };
 
   return (
-    <Grid container>
+    <Grid container spacing={2}>
       <Grid item xs={12} sm={6}>
-        <Typography align="center">
-          <pre>{JSON.stringify(regionInfo, null, 2)}</pre>
-        </Typography>
+        {regionInfo && <RegionCard regionInfo={regionInfo} />}
       </Grid>
       <Grid item xs={12} sm={6}>
         <RadioSVGMap map={Italy} onLocationMouseOver={handleMouseOver} />
