00f9e3ccc5da54ece1b0517dff63c50d90223a58
Gregory Chen <gregorychen3@gmail.com>
2020-09-14 01:16:07 -0400

    use region info interface

--- src/features/regions/ExploreRegions.tsx
+++ src/features/regions/ExploreRegions.tsx
@@ -3,20 +3,24 @@ import Italy from "@svg-maps/italy";
 import React, { useState } from "react";
 import { RadioSVGMap } from "react-svg-map";
 
+interface RegionInfo {
+  name: string;
+}
+
 export default function ExploreRegions() {
-  const [region, setRegion] = useState<string | null>(null);
+  const [regionInfo, setRegionInfo] = useState<RegionInfo | undefined>();
 
   return (
     <Grid container>
       <Grid item xs={12} sm={6}>
-        <Typography align="center">{region}</Typography>
+        <Typography align="center">{regionInfo}</Typography>
       </Grid>
       <Grid item xs={12} sm={6}>
         <RadioSVGMap
           map={Italy}
           onLocationMouseOver={(e) => {
             const target: SVGGraphicsElement = e.target;
-            setRegion(target.getAttribute("name"));
+            setRegionInfo({ name: e.name });
           }}
         />
       </Grid>
