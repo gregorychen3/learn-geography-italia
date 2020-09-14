e196be2a397f98bbc1dbed97f6a164bfbba499d5
Gregory Chen <gregorychen3@gmail.com>
2020-09-14 00:15:47 -0400

    add svg map

--- src/pages/RegionsPage.tsx
+++ src/pages/RegionsPage.tsx
@@ -1,6 +1,15 @@
-import Container from "@material-ui/core/Container";
+import { Container } from "@material-ui/core";
+import Italy from "@svg-maps/italy";
 import React from "react";
+import { SVGMap } from "react-svg-map";
+import "react-svg-map/lib/index.css";
 
-export default function RegionsPage() {
-  return <Container maxWidth="md">regions page</Container>;
+export default class App extends React.Component {
+  render() {
+    return (
+      <Container maxWidth="md">
+        <SVGMap map={Italy} />
+      </Container>
+    );
+  }
 }
