5e54ac36595747783b39b662a6ce4c2012de4022
Gregory Chen <gregorychen3@gmail.com>
2020-09-13 23:30:44 -0400

    regions page skel

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import AppHeader from "./components/AppHeader";
 import OverviewPage from "./pages/OverviewPage";
+import RegionsPage from "./pages/RegionsPage";
 
 export default function App() {
   return (
@@ -12,6 +13,9 @@ export default function App() {
       <Switch>
         <Route path="/overview">
           <OverviewPage />
+        </Route>
+        <Route path="/regions">
+          <RegionsPage />
         </Route>
         <Route path="/">
           <Redirect to="/overview" />
