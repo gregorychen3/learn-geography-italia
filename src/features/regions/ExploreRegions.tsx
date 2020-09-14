80cf7eda9454a65d9bef353896013b441d409b76
Gregory Chen <gregorychen3@gmail.com>
2020-09-14 02:13:16 -0400

    spacing

--- src/features/regions/ExploreRegions.tsx
+++ src/features/regions/ExploreRegions.tsx
@@ -25,7 +25,7 @@ export default function ExploreRegions() {
   };
 
   return (
-    <Grid container spacing={2}>
+    <Grid container spacing={4}>
       <Grid item xs={12} sm={6}>
         {regionInfo && <RegionInfo regionInfo={regionInfo} />}
       </Grid>
