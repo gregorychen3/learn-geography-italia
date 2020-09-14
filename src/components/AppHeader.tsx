733599dad5b94924c0eef3fc8d8f14fdd86b84eb
Gregory Chen <gregorychen3@gmail.com>
2020-09-13 23:46:26 -0400

    click header icon to go home

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,9 +1,10 @@
-import { makeStyles } from "@material-ui/core";
+import { IconButton, makeStyles } from "@material-ui/core";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import ExploreIcon from "@material-ui/icons/Explore";
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   icon: { marginRight: theme.spacing(2) },
@@ -11,11 +12,15 @@ const useStyles = makeStyles((theme) => ({
 
 export default function AppHeader() {
   const classes = useStyles();
+  const h = useHistory();
 
   return (
     <AppBar position="relative">
       <Toolbar>
-        <ExploreIcon className={classes.icon} />
+        <IconButton edge="start" className={classes.icon} color="inherit" onClick={() => h.push("/")}>
+          <ExploreIcon />
+        </IconButton>
+
         <Typography variant="h6" color="inherit" noWrap>
           Geografia dell'Italia
         </Typography>
