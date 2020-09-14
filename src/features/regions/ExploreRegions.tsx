55fa4489549b3b22e7954d18c8578f6b37ef6d5c
Gregory Chen <gregorychen3@gmail.com>
2020-09-14 01:09:44 -0400

    region onLocationMouseOver

--- src/features/regions/ExploreRegions.tsx
+++ src/features/regions/ExploreRegions.tsx
@@ -1,16 +1,24 @@
 import { Grid, Typography } from "@material-ui/core";
 import Italy from "@svg-maps/italy";
-import React from "react";
+import React, { useState } from "react";
 import { RadioSVGMap } from "react-svg-map";
 
 export default function ExploreRegions() {
+  const [region, setRegion] = useState<string | null>(null);
+
   return (
     <Grid container>
       <Grid item xs={12} sm={6}>
-        <Typography align="center">foo</Typography>
+        <Typography align="center">{region}</Typography>
       </Grid>
       <Grid item xs={12} sm={6}>
-        <RadioSVGMap map={Italy} />
+        <RadioSVGMap
+          map={Italy}
+          onLocationMouseOver={(e) => {
+            const target: SVGGraphicsElement = e.target;
+            setRegion(target.getAttribute("name"));
+          }}
+        />
       </Grid>
     </Grid>
   );
