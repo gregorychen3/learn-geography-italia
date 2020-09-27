3cd54b9d3de28f23cc68be5d31319ca27396521f
Gregory Chen <gchen213@bloomberg.net>
2020-09-27 12:37:23 -0400

    renaming

--- src/features/regions/MultipleChoiceQuiz.tsx
+++ src/features/regions/MultipleChoiceQuiz.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
-
 import MultipleChoiceForm from "./MultipleChoiceForm";
 
 export default function MultipleChoiceQuiz() {
   const opts = ["a", "b", "c"];
   return (
     <>
-      <MultipleChoiceForm regionOpts={opts} />
+      <MultipleChoiceForm opts={opts} />
     </>
   );
 }
